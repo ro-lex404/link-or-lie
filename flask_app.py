b44aa029b894a932b50c5d28354f2079456261d5
rohan <rohanalex135@gmail.com>
2025-09-01 21:15:27 +0530

    Added styling for elements

--- flask_app.py
+++ flask_app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/phish-link-check')
+@app.route('/')
 def phish():
     return render_template("index.html")
 
 @app.route('/spot-the-phish')
 def game():
-    return render_template("spot-the-fish.html")
+    return render_template("spot-the-phish.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
