36373be6ff01493df00008ab12a1b561bdf1aaa7
rohan <rohanalex135@gmail.com>
2025-09-04 11:57:10 +0530

    major changes in css, modified spot-the-phish

--- flask_app.py
+++ flask_app.py
@@ -13,9 +13,12 @@ def game():
 
 @app.route('/process-form', methods=["POST"])
 def form():
-    link = request.form.get("link")
-    retVal = linkModel.model(link)
-    return retVal
+    try:
+        link = request.form.get("link")
+        retVal = linkModel.model(link)
+        return retVal
+    except ConnectError as e:
+        return "Please check your internet connection and try again."
 
 if __name__ == "__main__":
     app.run(debug=True)
